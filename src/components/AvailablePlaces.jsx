3fa8ebda993f77e269b4ae514b9ecef011fd4904
Yousef Na <yousefnasr0541@gmail.com>
2023-11-11 08:28:15 +0200

    send HTTP request

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,37 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+	const [AvailablePlaces, setAvailablePlaces] = useState([]);
+	const [isFetching, setIsFetching] = useState(false);
+	const [error, setError] = useState();
+
+	useEffect(() => {
+		async function fetchPlaces() {
+			setIsFetching(true);
+			try {
+				const response = await fetch("http://localhost:3000");
+				if (!response.ok) {
+					throw new Error(`Failed to fetch places`);
+				}
+				const resData = await response.json();
+				setAvailablePlaces(resData.places);
+			} catch (error) {
+				setError(error);
+			}
+			setIsFetching(false);
+		}
+		fetchPlaces();
+	}, []);
+
+	return (
+		<Places
+			title="Available Places"
+			places={AvailablePlaces}
+			isLoading={isFetching}
+			loadingText="Fetching places data..."
+			fallbackText="No places available."
+			onSelectPlace={onSelectPlace}
+		/>
+	);
 }
