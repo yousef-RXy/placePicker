5cf780696296a59308ca35d491c93cc2d80de070
Yousef Na <yousefnasr0541@gmail.com>
2023-11-14 09:14:28 +0200

    Send data to back end

--- src/HTTP.js
+++ src/HTTP.js
@@ -1,11 +1,25 @@
 export async function fetchAvailablePlaces() {
 	const response = await fetch("http://localhost:3000/places");
 
-	if (!response.ok) {
-		throw new Error(`Failed to fetch places`);
-	}
+	if (!response.ok) throw new Error(`Failed to fetch places`);
 
 	const resData = await response.json();
 
 	return resData.places;
+}
+
+export async function updateAvailablePlaces(places) {
+	const response = await fetch("http://localhost:3000/user-places", {
+		method: "PUT",
+		body: JSON.stringify({ places }),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	if (!response.ok) throw new Error(`Failed to updat user places`);
+
+	const resData = await response.json();
+
+	return resData.message;
 }
